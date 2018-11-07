82c5e0845ce9a507a79375d2b67741a4c99501d4
Patrick Henschel <melchor@gmx.at>
2018-11-07 10:45:57 +0100

    initial project commit

---
+++ ARFoundation/Assets/ARManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARManager : MonoBehaviour
+{
+    [SerializeField] private ARSession arSession;
+
+    private void Awake()
+    {
+        ARSubsystemManager.systemStateChanged += ARSubsystemManager_systemStateChanged;
+    }
+
+    private void OnDestroy()
+    {
+        ARSubsystemManager.systemStateChanged -= ARSubsystemManager_systemStateChanged;
+    }
+
+    private void ARSubsystemManager_systemStateChanged(ARSystemStateChangedEventArgs newState)
+    {
+        
+    }
+}
