f8659bb91b529d6e60db366fbdd1490da3fd7f06
Patrick Henschel <melchor@gmx.at>
2018-12-03 15:35:52 +0100

    adding touch control

--- ARFoundation/Assets/ARManager.cs
+++ ARFoundation/Assets/ARManager.cs
@@ -8,8 +8,11 @@ public class ARManager : MonoBehaviour
     [SerializeField] private ARSessionOrigin arSessionOrigin;
     [SerializeField] private GameObject raycastHitGameObject;
 
+    private GameObject spawnedObject;
 
     private List<ARRaycastHit> arRaycastHits = new List<ARRaycastHit>();
+    private List<GameObject> augmentedObjects = new List<GameObject>();
+    private bool enableTouchControl;
 
     private void Awake()
     {
@@ -26,48 +29,64 @@ public class ARManager : MonoBehaviour
         
     }
 
+    private void Update()
+    {
+        if (arSessionOrigin != null && enableTouchControl && Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
 
-    //private void Update()
-    //{
-    //    if (arSessionOrigin != null)
-    //    {
-    //        arRaycastHits.Clear();
+            arRaycastHits.Clear();
 
-    //        var raycastHits = arSessionOrigin.Raycast(Vector3.zero, arRaycastHits);
-    //        if (raycastHits)
-    //        {
-
-    //        }
-    //    }
-    //}
+            var raycastHits = arSessionOrigin.Raycast(touch.position, arRaycastHits);
+            if (raycastHits)
+            {
+                Debug.Log("Touch input hits: " + arRaycastHits.Count + " colliders.");
+                for (int i = 0; i < arRaycastHits.Count; i++)
+                {
+                    var raycastHit = arRaycastHits[i];
+                    var hitPose = raycastHit.pose;
+                    
+                    if (spawnedObject == null)
+                    {
+                        spawnedObject = Instantiate(raycastHitGameObject, hitPose.position, hitPose.rotation);
+                        Debug.Log("Touch input spawns object at: " + hitPose.position + "-" + hitPose.rotation);
+                    }
+                    else
+                    {
+                        spawnedObject.transform.position = hitPose.position;
+                        Debug.Log("Touch input moves object to: " + hitPose.position);
+                    }
+                }
+            }
+        }
+    }
 
     private void OnGUI()
     {
-        if (arSession != null && !arSession.enabled && GUI.Button(new Rect(100, 10, 100, 60), "enable"))
+        if (arSession != null && !arSession.enabled && GUI.Button(new Rect(100, 10, 200, 120), "enable"))
         {
             arSession.enabled = true;
         }
-        else if (arSession != null && arSession.enabled && GUI.Button(new Rect(10, 100, 100, 60), "disable"))
+        else if (arSession != null && arSession.enabled && GUI.Button(new Rect(10, 100, 200, 120), "disable"))
         {
             arSession.enabled = false;
         }
 
-        if (arSession != null && GUI.Button(new Rect(100, 80, 100, 60), "reset"))
+        if (arSession != null && GUI.Button(new Rect(100, 150, 200, 120), "reset"))
         {
             arSession.Reset();
-            
         }
 
-        if (arSession != null && !arSession.lightEstimation && GUI.Button(new Rect(100, 150, 100, 60), "en. light est."))
+        if (arSession != null && !arSession.lightEstimation && GUI.Button(new Rect(100, 290, 200, 120), "en. light est."))
         {
             arSession.lightEstimation = true;
         }
-        else if (arSession != null && arSession.lightEstimation && GUI.Button(new Rect(100, 150, 100, 60), "dis. light est."))
+        else if (arSession != null && arSession.lightEstimation && GUI.Button(new Rect(100, 290, 200, 120), "dis. light est."))
         {
             arSession.lightEstimation = false;
         }
 
-        if (arSession != null && GUI.Button(new Rect(100, 220, 100, 60), "cast ray"))
+        if (!enableTouchControl && arSession != null && GUI.Button(new Rect(100, 360, 200, 120), "cast ray"))
         {
             arSessionOrigin.Raycast(Vector3.zero, arRaycastHits);
             Debug.Log(arRaycastHits.Count + " objects hit.");
@@ -77,13 +96,14 @@ public class ARManager : MonoBehaviour
                 var relativePose = raycastHit.sessionRelativePose;
                 var pose = raycastHit.pose;
                 var augmentedObject = Instantiate(raycastHitGameObject, pose.position, pose.rotation);
+                Debug.Log("place object at pose: " + pose.position + "-" + pose.rotation + " relative pose: " + relativePose.position + "-" + relativePose.rotation);
                 augmentedObjects.Add(augmentedObject);
             }
 
             arRaycastHits.Clear();
         }
 
-        if (arSession != null && GUI.Button(new Rect(100, 290, 100, 60), "clear"))
+        if (arSession != null && GUI.Button(new Rect(100, 500, 200, 120), "clear"))
         {
             for (int i = 0; i < augmentedObjects.Count; i++)
             {
@@ -91,7 +111,10 @@ public class ARManager : MonoBehaviour
             }
             augmentedObjects.Clear();
         }
-    }
 
-    private List<GameObject> augmentedObjects = new List<GameObject>();
+        if (arSession != null && GUI.Button(new Rect(100, 500, 200, 120), "Touch: " + enableTouchControl))
+        {
+            enableTouchControl = !enableTouchControl;
+        }
+    }
 }
